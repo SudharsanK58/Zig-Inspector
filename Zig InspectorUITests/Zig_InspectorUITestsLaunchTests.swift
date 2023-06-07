4ddebeb05c9f24010b2b9e0b8d22bf744a1427df
Sudhar San <34421911+SudharsanK58@users.noreply.github.com>
2023-06-07 22:01:28 +0530

    Initial Commit

---
+++ Zig InspectorUITests/Zig_InspectorUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Zig_InspectorUITestsLaunchTests.swift
+//  Zig InspectorUITests
+//
+//  Created by Sudharsan on 07/06/23.
+//
+
+import XCTest
+
+final class Zig_InspectorUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
