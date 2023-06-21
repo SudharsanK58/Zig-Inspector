80fa72248a2bebda0cf898ca11d3b0e287353db6
Sudhar San <34421911+SudharsanK58@users.noreply.github.com>
2023-06-21 23:01:18 +0530

    Added new TableViewController screen and new cell
    Added new TableViewController screen and new cell

---
+++ Zig Inspector/VenuesListScreen/VenuesTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  VenuesTableViewCell.swift
+//  Zig Inspector
+//
+//  Created by Sudharsan on 20/06/23.
+//
+
+import UIKit
+
+class VenuesTableViewCell: UITableViewCell {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }    
+}
+
