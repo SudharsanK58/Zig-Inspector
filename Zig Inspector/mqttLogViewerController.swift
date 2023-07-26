ba74bd32916b21eef5b35722580c5ac4df109a2f
Sudhar San <34421911+SudharsanK58@users.noreply.github.com>
2023-07-27 00:30:51 +0530

    Added sample GPS

--- Zig Inspector/mqttLogViewerController.swift
+++ Zig Inspector/mqttLogViewerController.swift
@@ -7,11 +7,14 @@
 
 import UIKit
 import CocoaMQTT
+import MapKit
 
 class mqttLogViewerController: UIViewController {
     let defaultHost = "mqtt.zig-web.com"
 
     var mqtt: CocoaMQTT?
+    
+    @IBOutlet weak var deviceMapViewer: MKMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -37,15 +40,15 @@ class mqttLogViewerController: UIViewController {
 extension mqttLogViewerController: CocoaMQTTDelegate {
     
     func mqtt(_ mqtt: CocoaMQTT, didConnectAck ack: CocoaMQTTConnAck) {
-            print("ack: \(ack)")
-            if ack == .accept {
-                print("Successfully connected to MQTT")
-                mqtt.subscribe("chat/", qos: CocoaMQTTQoS.qos1)
-                mqtt.publish("Nie", withString: "Hai")
-            } else {
-                print("Failed to connect to MQTT: \(ack)")
-            }
+        print("ack: \(ack)")
+        if ack == .accept {
+            print("Successfully connected to MQTT")
+            mqtt.subscribe("map/log", qos: CocoaMQTTQoS.qos1)
+        } else {
+            print("Failed to connect to MQTT: \(ack)")
         }
+    }
+
     func mqtt(_ mqtt: CocoaMQTT, didStateChangeTo state: CocoaMQTTConnState) {
         print("new state: \(state)")
     }
@@ -60,7 +63,26 @@ extension mqttLogViewerController: CocoaMQTTDelegate {
 
     func mqtt(_ mqtt: CocoaMQTT, didReceiveMessage message: CocoaMQTTMessage, id: UInt16 ) {
         print("message: \(message.string!.description), id: \(id)")
+        if message.topic == "map/log" {
+            if let msgString = message.string, msgString == "AM" {
+                // Coordinates for somewhere in America
+                let americaCoordinates = CLLocationCoordinate2D(latitude: 37.7749, longitude: -122.4194) // This is for San Francisco
+                let region = MKCoordinateRegion(center: americaCoordinates, latitudinalMeters: 50000, longitudinalMeters: 50000)
+                
+                // Update the map on the main thread
+                DispatchQueue.main.async {
+                    self.deviceMapViewer.setRegion(region, animated: true)
+
+                    // Add an annotation (marker)
+                    let annotation = MKPointAnnotation()
+                    annotation.coordinate = americaCoordinates
+                    self.deviceMapViewer.addAnnotation(annotation)
+                }
+            }
+        }
     }
+
+
 
     func mqtt(_ mqtt: CocoaMQTT, didSubscribeTopics success: NSDictionary, failed: [String]) {
         print("subscribed: \(success), failed: \(failed)")
